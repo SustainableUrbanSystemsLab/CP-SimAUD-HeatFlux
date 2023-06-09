f89d524cd713f7e9de025ff7af67a6fb217f00a8
Patrick Kastner <patrick.kastner@gmail.com>
2023-06-09 14:33:49 -0400

    Fix for run_heatflux.bat

--- run_heatflux.bat
+++ run_heatflux.bat
@@ -1,3 +1,21 @@
-docker run -v "%cd%:/home/openfoam" --entrypoint="" hfdresearch/swak4foamandpyfoam:latest-v4.1 bash -c "source /opt/openfoam4/etc/bashrc && cd /home/openfoam &&  postProcess -func grad(T) -region zone_0 &&  postProcess -func grad(T) -region zone_1 &&  postProcess -func grad(T) -region zone_2 &&  postProcess -func grad(T) -region zone_3 &&  postProcess -func grad(T) -region zone_4 &&  postProcess -func grad(T) -region zone_5 &&  postProcess -func grad(T) -region zone_6 &&  postProcess -func grad(T) -region zone_7 &&  postProcess -func grad(T) -region zone_8 &&  postProcess -func grad(T) -region zone_9 &&  postProcess -func grad(T) -region zone_11 &&  postProcess -func grad(T) -region zone_12 &&  postProcess -func grad(T) -region zone_10 &&  postProcess -func grad(T) -region zone_13 && "
+setlocal
+call "C:\Program Files\blueCFD-Core-2017\\setvars.bat"
+set PATH=%HOME%\msys64\usr\bin;%PATH%
+cd /d %~dp0
+postProcess -func grad(T) -region zone_0
+postProcess -func grad(T) -region zone_1
+postProcess -func grad(T) -region zone_2
+postProcess -func grad(T) -region zone_3
+postProcess -func grad(T) -region zone_4
+postProcess -func grad(T) -region zone_5
+postProcess -func grad(T) -region zone_6
+postProcess -func grad(T) -region zone_7
+postProcess -func grad(T) -region zone_8
+postProcess -func grad(T) -region zone_9
+postProcess -func grad(T) -region zone_11
+postProcess -func grad(T) -region zone_12
+postProcess -func grad(T) -region zone_10
+postProcess -func grad(T) -region zone_13
+"
 
 PAUSE
